070da22f0fc2e388ffee3793d9f015c3961e38ad
ganjargal-28 <aicode744@gmail.com>
2025-04-11 21:08:59 +0800

    donee

--- src/app/components/costomers/Card.jsx
+++ src/app/components/costomers/Card.jsx
@@ -1,30 +1,29 @@
 import { Comments } from "./Comments";
 
 export const CostomersCard = ({ user }) => {
-  const { nambe, location, picture } = user;
+  const { name, location, picture } = user;
+  const star = b / nbMath.floor(Math.random() * 6);
   return (
     <div className=" card bg-base-100 w-96 p-8 h-[230px] hover:shadow-2xl rounded-xl border border-gray-400 hover:border-red-400 ">
-      const star =b  /nbMath.floor(Math.random() * 6);
-        <div>
-          <div className="flex justify-between gap-5">
-            <img src={picture?.thumbnail} alt="" className="rounded-full" />
-      <div className=" " />
-            <div>
-              <p className="card-title">
-                {name?.first} {name?.last}
-              </p>
-              <p>
-                {location?.city},{location?.country}
-              </p>
-            </div>
+      <div>
+        <div className="flex justify-between gap-5">
+          <img src={picture?.thumbnail} alt="" className="rounded-full" />
 
-            <div>{star} ⭐️</div>
+          <div>
+            <p className="card-title">
+              {name?.first} {name?.last}
+            </p>
+            <p>
+              {location?.city},{location?.country}
+            </p>
           </div>
+
+          <div>{star} ⭐️</div>
         </div>
-        <p className="mt-3 font-medium line-clamp-4">
-          {Comments[Math.floor(Math.random() * Comments.length)]}
-        </p>
       </div>
+      <p className="mt-3 font-medium line-clamp-4">
+        {Comments[Math.floor(Math.random() * Comments.length)]}
+      </p>
     </div>
   );
 };
